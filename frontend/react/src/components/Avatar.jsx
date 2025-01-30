b41e97261d230e5b50d827f31c7700f153ec0c3d
fahad <fk336733@gmail.com>
2025-01-31 01:48:00 +0500

    made UX more beautiful and cross-platform compatible

--- frontend/react/src/components/Avatar.jsx
+++ frontend/react/src/components/Avatar.jsx
@@ -1,7 +1,7 @@
 export const Avatar = ({rest}) => {
     return (
 
-        <img className={`${rest} w-11 h-11 p-1 rounded-full ring-2 ring-gray-300 dark:ring-green-800`}
+        <img className={`${rest} w-9 h-9  md:w-11 md:h-11 p-1 rounded-full ring-2 ring-gray-300 dark:ring-green-800`}
              src={"https://avatars.githubusercontent.com/u/110169794?v=4"}
              alt="Bordered avatar"/>
 
